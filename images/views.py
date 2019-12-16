6737b5e4aa074a54c95ca5ddb2c4a81afb4ed11f
yvette <yvetteadan2000@gmail.com>
2019-12-16 10:17:32 +0300

    add definitions

--- images/views.py
+++ images/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
-.
+
 def photos(request):
-    return  render(request, 'photo.html')
+    return  render(request, 'photos.html')
 def search_results(request):
     search = dt.image.get()
     return render(request, 'all-images/search.html')
-
+    
+def location(request):
+    return render(request, 'photos.html')
