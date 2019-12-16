4ad871cdaa717c3e2ed93cc16aefe308256ed6ee
yvette <yvetteadan2000@gmail.com>
2019-12-16 18:26:12 +0300

    add search results

--- images/urls.py
+++ images/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [ 
-    url('^$', views.photos , name='photos'),
-    url('^$', views.photos , name='navbar')
-] 
+    url('^$', views.photos,name='photos'),
+    url(r'^search/', views.search_results, name='search_results')] 
