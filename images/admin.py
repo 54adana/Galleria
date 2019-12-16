e5660abc43e2fdb4d7bcd396b1fcd894291b6a76
yvette <yvetteadan2000@gmail.com>
2019-12-16 10:15:36 +0300

    add class

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category,Image
+
 
 # Register your models here.
+
+
+class ArticleAdmin(admin.ModelAdmin):
+
+
+    admin.site.register(Category)
+    admin.site.register(Image)
+
+
