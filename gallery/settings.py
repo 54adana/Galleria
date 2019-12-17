cac1375c5f75e821b82d91e0f6b21856e4caa7fb
yvette <yvetteadan2000@gmail.com>
2019-12-17 13:22:32 +0300

    change config

--- gallery/settings.py
+++ gallery/settings.py
@@ -66,7 +66,7 @@ else:
 
 INSTALLED_APPS = [
     'bootstrap3',
-    'images.apps.ImagesConfig',
+    'images',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
